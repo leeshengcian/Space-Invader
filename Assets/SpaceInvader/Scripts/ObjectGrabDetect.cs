6db6b237cd1db03c6ed6b623e000303cc5c30677
TankLee <sclee.cs12@nycu.edu.tw>
2023-10-17 16:05:45 +0800

    add arrow pointing animation to gun

--- Assets/SpaceInvader/Scripts/ObjectGrabDetect.cs
+++ Assets/SpaceInvader/Scripts/ObjectGrabDetect.cs
@@ -6,6 +6,7 @@ public class ObjectGrabDetect : MonoBehaviour
 {
     // check whether the object is being grabbed
     public bool GunGrabbed;
+    public GameObject Arrow;
 
     // check whether hand is grabbing object
     private OVRGrabbable ovrGrab;
@@ -14,11 +15,17 @@ public class ObjectGrabDetect : MonoBehaviour
     private void Start()
     {
         ovrGrab = GetComponent<OVRGrabbable>();
+        Arrow.GetComponent<Animation>().Play();
     }
 
     private void Update()
     {
-        if (ovrGrab.isGrabbed) isGrabGun = true;
+        Arrow.transform.position = transform.position;
+        Arrow.SetActive(true);
+        if (ovrGrab.isGrabbed) {
+            isGrabGun = true;
+            Arrow.SetActive(false);
+        } 
         else isGrabGun = false;
 
         GunGrabbed = isGrabGun;
